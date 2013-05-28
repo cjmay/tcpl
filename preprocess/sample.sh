481f36d45f2b3ef15b73ce1a511db3c12e723d0d
Chandler May <cjmay4754@gmail.com>
2013-05-27 18:17:57 -0700

    sample arg check

--- preprocess/sample.sh
+++ preprocess/sample.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]
+then
+    echo 'Specify a username and password.'
+    exit 1
+fi
+
 wget --user="$1" --password="$2" --read-timeout=0 https://stream.twitter.com/1.1/statuses/sample.json
